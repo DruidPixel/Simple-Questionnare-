b9cc4c58130b424fb7dd2d842780e7672c2a7ba9
DruidPixel <74475018+DruidPixel@users.noreply.github.com>
2021-03-18 11:09:10 +0000

    Add files via upload

---
+++ GitHub Questionnare.py
@@ -0,0 +1,40 @@
+#Question 1
+score = 0
+print("*Your Question Goes Here*")
+print("a. answer 1")
+print("b. answer 2")
+print("c. answer 2")
+
+q1_ans = input("Your answer")
+
+if q1_ans == "a" or q1_ans == "A":
+    print("Correct")
+    score = score + 1
+else:
+    print("Incorrect the answer is A.")
+
+    print(" ")
+    print(" ")
+    print(" ")
+
+    
+#Question 2
+print("*Your Question Goes Here*")
+print("a. answer 1")
+print("b. answer 2")
+print("c. answer 2")
+
+q1_ans = input("Your answer")
+
+if q1_ans == "a" or q1_ans == "A":
+    print("Correct")
+    score = score + 1
+else:
+    print("Incorrect the answer is A.")
+
+print("Your Score:")
+print("|")
+print("v")
+print(score)
+
+    
